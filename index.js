9080561d57db75566b56a1db306fe4c63f10de86
Nallaiah Mahendramoorthy <nalamahen@nallaiahs-mbp.home>
2020-11-29 18:11:46 +0000

    simple node app with a test

---
+++ index.js
@@ -0,0 +1,16 @@
+//importing node framework
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('server started on port:', PORT);
+});
+
+module.exports = app;
